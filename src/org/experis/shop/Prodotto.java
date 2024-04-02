d7e64773119a458dbe2c4f8f043026960252671a
AlessandroPirisinu <palazzialessandro2@gmail.com>
2024-04-02 20:51:35 +0200

    add Product class with attributes + getters and setters

---
+++ src/org/experis/shop/Prodotto.java
@@ -0,0 +1,43 @@
+package org.experis.shop;
+
+import java.math.BigDecimal;
+
+public class Prodotto {
+    // Attributes
+    private int code;
+    private String name, brand;
+    BigDecimal price , vat;
+
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public BigDecimal getVat() {
+        return vat;
+    }
+
+    public void setVat(BigDecimal vat) {
+        this.vat = vat;
+    }
+}
