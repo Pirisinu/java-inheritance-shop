8be51bf0079fe154f2d7d609c018aaa34e5703d2
AlessandroPirisinu <palazzialessandro2@gmail.com>
2024-04-02 23:54:16 +0200

    add Televisions Attributes,Constructor,Methods Override, Getters e Setters

--- src/org/experis/shop/products/Televisions.java
+++ src/org/experis/shop/products/Televisions.java
@@ -3,9 +3,52 @@ package org.experis.shop.products;
 import org.experis.shop.Prodotto;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class Televisions extends Prodotto {
-    public Televisions(String name, String brand, BigDecimal price, BigDecimal vat) {
+    // Attributes
+    private String dimensions;
+    private boolean isSmart;
+
+    // Constructor
+    public Televisions(String name, String brand, BigDecimal price, BigDecimal vat, String dimensions, boolean isSmart) {
         super(name, brand, price, vat);
+        this.dimensions = dimensions;
+        this.isSmart = isSmart;
+    }
+    // Methods Override
+    @Override
+    public BigDecimal getVatPrice(){
+        return this.getPrice().add(this.getPrice().divide(new BigDecimal(100)).multiply(this.getVat())).setScale(2, RoundingMode.HALF_EVEN);
+    }
+
+    @Override
+    public String getFullInfo(){
+        return "Product n°" + this.getCode() + " || "
+                + "Name: " + this.getName() + " || "
+                + "Brand: " + this.getBrand() + " || "
+                + "Original price: " + this.getEuroFormatter().format(this.getPrice()) + " || "
+                + "Vat price: " + this.getEuroFormatter().format(getVatPrice()) + " || ";
+    }
+
+    // Getters e Setters
+
+
+    public String getDimensions() {
+        return dimensions;
+    }
+
+    public void setDimensions(String dimensions) {
+        this.dimensions = dimensions;
+    }
+
+    public String isSmart() {
+        if (isSmart){
+            return "Smart device";
+        }else return "Not smart device";
+    }
+
+    public void setSmart(boolean smart) {
+        isSmart = smart;
     }
 }
