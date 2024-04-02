361e7ffa783583493197274c86ce951a61f0c69d
AlessandroPirisinu <palazzialessandro2@gmail.com>
2024-04-02 22:09:53 +0200

    add Headphonse Attributes,Constructor,Methods Override, Getters e Setters

--- src/org/experis/shop/products/Headphones.java
+++ src/org/experis/shop/products/Headphones.java
@@ -3,9 +3,48 @@ package org.experis.shop.products;
 import org.experis.shop.Prodotto;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class Headphones extends Prodotto {
-    public Headphones(String name, String brand, BigDecimal price, BigDecimal vat) {
+    // Attributes
+    private String color;
+    private boolean isWireless;
+
+    // Constructor
+    public Headphones(String name, String brand, BigDecimal price, BigDecimal vat, String color, boolean isWireless) {
         super(name, brand, price, vat);
+        this.color = color;
+        this.isWireless = isWireless;
+    }
+    // Methods Override
+    @Override
+    public BigDecimal getVatPrice(){
+        return this.getPrice().add(this.getPrice().divide(new BigDecimal(100)).multiply(this.getVat())).setScale(2, RoundingMode.HALF_EVEN);
+    }
+
+    @Override
+    public String getFullInfo(){
+        return "Product n°" + this.getCode() + " || "
+                + "Name: " + this.getName() + " || "
+                + "Brand: " + this.getBrand() + " || "
+                + "Original price: " + this.getEuroFormatter().format(this.getPrice()) + " || "
+                + "Vat price: " + this.getEuroFormatter().format(getVatPrice()) + " || ";
+    }
+    // Getters e Setters
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isWireless() {
+        return isWireless;
+    }
+
+    public void setWireless(boolean wireless) {
+        isWireless = wireless;
     }
 }
